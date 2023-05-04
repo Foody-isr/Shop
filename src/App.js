38e33635d3dfbe72b2970d9dd81ca709f6422ccf
Mickael Zana <zanamickael@gmail.com>
2023-05-04 17:26:55 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import awsExports from "./aws-exports";
 import {
   StyledEngineProvider,
   ThemeProvider,
